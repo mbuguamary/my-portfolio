e7c3fb75bef48386394f70e592d8fc22d554a45f
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-13 21:23:33 +0300

    added my description

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#332c94f1]'>
+      <p className='text-'>Hi my name is</p>
+      <h1>Mary Wairimu Mbugua</h1>
+      <h2>I'm a full Stack Developer</h2>
+      <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building responsive full-stack web applications.
+        </p>
+       </div>
+  )
+}
+
+export default Home
