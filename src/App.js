7403870e5f165d00e83546da8f625b6c4880c8f5
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-17 15:37:42 +0300

    linked the skills page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Skills from "./components/Skills"
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
